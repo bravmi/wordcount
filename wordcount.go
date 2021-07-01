6679def0bb694c59886061343bd9c7050739084d
Michael T <bravmech@gmail.com>
2021-07-01 11:39:42 +0500

    refactor

--- wordcount.go
+++ wordcount.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+	if len(os.Args) != 2 {
+        fmt.Println("usage: wordcount <sentence>")
+        os.Exit(1)
+    }
 	s := strings.Trim(os.Args[1], "\"'")
 	words := strings.Fields(s)
 	fmt.Println(len(words))
